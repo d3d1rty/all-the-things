7ee6e5aae3775b5e3b2170b9f0989171f79f04e0
Dick Davis <dick@hey.com>
2021-11-10 09:14:58 -0600

    Modify config for Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,3 +118,13 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: ENV['APP_DOMAIN'],
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
