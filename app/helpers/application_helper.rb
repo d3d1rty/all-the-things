245a82bbcc890d29771a2e0a69a5ac12c45045ed
Dick Davis <dick@hey.com>
2021-11-10 00:06:42 -0600

    Enumerate classes to keep from purging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,5 @@
 ##
 # Base helpers for views
 module ApplicationHelper
+  KEEP_THESE_CLASSES = 'bg-red-500 border-red-500 text-red-500 hover:bg-red-500 hover:bg-red-700 bg-red-500 border-blue-500 text-blue-500 hover:bg-blue-500 hover:bg-blue-700'
 end
