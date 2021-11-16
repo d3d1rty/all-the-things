260d92590ea8c449cffd9ec9c37f0c218813271f
Dick Davis <dick@hey.com>
2021-11-16 11:32:21 -0600

    Refactor navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@
 ##
 # Base class for controllers
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+     items_path
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to items_path if user_signed_in?
   end
 end
