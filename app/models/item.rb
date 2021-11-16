2c542c6e24584067093b6fa9220bafabe1089ce0
Dick Davis <dick@hey.com>
2021-11-16 02:49:59 -0600

    UI/UX improvements (#7)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,28 +1,37 @@
 class Item < ApplicationRecord
   has_one :loan
 
+  validate :stocked_at_not_in_future
   validates :name, presence: true, length: { maximum: 25 }
   validates :sku, presence: true, length: { maximum: 20 }
   validates :price, presence: true,
                     numericality: {
                       greater_than_or_equal_to: 0,
-                      less_than: BigDecimal(10**7)
+                      less_than: BigDecimal(10**9)
                     },
                     format: {
-                      with: /\A\d{1,3}(\.\d{1,2})?\z/
+                      with: /\A\d{1,6}(\.\d{1,2})?\z/
                     }
   validates :cost, presence: true,
                    numericality: {
                      greater_than_or_equal_to: 0,
-                     less_than: BigDecimal(10**7)
+                     less_than: BigDecimal(10**9)
                    },
                    format: {
-                     with: /\A\d{1,3}(\.\d{1,2})?\z/
+                     with: /\A\d{1,6}(\.\d{1,2})?\z/
                    }
 
   def days_in_stock
     return "" unless stocked_at.present?
 
     ((sold_at&.to_date || DateTime.current) - stocked_at.to_date).to_i
+  end
+
+  private
+
+  def stocked_at_not_in_future
+    if stocked_at.present? && stocked_at.future?
+      errors.add(:stocked_at, 'cannot be in the future')
+    end
   end
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,6 +2,8 @@ class Loan < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
+  after_update :release_item, if: :saved_change_to_default_date?
+
   validates :principal, presence: true,
                         numericality: {
                           greater_than_or_equal_to: 0,
@@ -20,4 +22,9 @@ class Loan < ApplicationRecord
                               with: /\A\d{1,3}(\.\d{1,2})?\z/
                             }
   validates :maturity_date, presence: true
+
+  def release_item
+    item.released_at = Time.now
+    item.save
+  end
 end
